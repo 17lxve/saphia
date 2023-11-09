76c39e8bc86e453cf0ad65991536e36c7e617656
17lxve <timmydavidsonkb@gmail.com>
2023-11-09 18:10:35 +0000

    idk what i'm doing frfr

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,16 +1,19 @@
-type Props = {tabs:{name:string, id:number}[]}
+import { ReactNode } from "react"
+
+type Props = {tabs:{name:string,icon:ReactNode, id:number}[]}
 
 function Sidebar({tabs}:Props) {
     return (
       <>
           <main className="sidebar">
               <div>
-                  <h1> Sidebar </h1>
+                  <h1 className="sidebar-element"> Chatbot <img src="/images/gs2e1.png" alt="" /> </h1>
                   <div>{
                     tabs.map(
                         (tab) => (
                             <div className="sidebar-element" key={tab.id}>
-                                <h2>{tab.name}</h2>
+                              {tab.icon}
+                                <h2 className="links">{tab.name}</h2>
                             </div>
                         )
                     )
