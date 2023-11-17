77197a841b3364c8c5cfc4540de0ae50c940c1f0
KOUADIO-BHEGNIN OTHNIEL KUAWO ANATOLE <othniel.kouadio@gs2e.ci>
2023-11-17 16:30:00 +0000

    log

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,12 +1,42 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import * as Form from "@radix-ui/react-form";
 import * as Checkbox from "@radix-ui/react-checkbox";
 import { CheckIcon, EnterIcon } from "@radix-ui/react-icons";
+import axios from 'axios'
+async function submitForm(data: { [k: string]: FormDataEntryValue; }){
+  const response = await axios.post('http://localhost:3000/login',data).catch((err) => console.log(err))
+  console.warn(response)
+  //@ts-ignore
+  if(response.data.err){
+    throw Error("Wrong credentials!")
+  } else {
+  //@ts-ignore
+  return(response.data)
+  }
+}
 function LoginComponent() {
   return (
     <>
       <div className="tabContainer">
-        <Form.Root>
-          <Form.Field name="email">
+        <Form.Root
+      // `onSubmit` only triggered if it passes client-side validation
+      onSubmit={(event) => {
+        const data = Object.fromEntries(new FormData(event.currentTarget));
+
+        // Submit form data and catch errors in the response
+        submitForm(data)
+          .then((res) => {console.log(res)})
+          /**
+           * Map errors from your server response into a structure you'd like to work with.
+           * In this case resulting in this object: `{ email: false, password: true }`
+           */
+          .catch((errors) => alert(errors.message));
+
+        // prevent default form submission
+        event.preventDefault();
+      }}
+    >
+          <Form.Field name="username">
             <div
               style={{
                 display: "flex",
